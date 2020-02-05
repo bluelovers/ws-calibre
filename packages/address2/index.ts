1425a42fbbf0ac8e3935d41b7671d7bfd66f53ce
bluelovers <codelovers@users.sourceforge.net>
2020-02-05 16:16:23 +0800

    .

--- packages/address2/index.ts
+++ packages/address2/index.ts
@@ -1,18 +1,19 @@
-import { ip as IPAddress } from 'address';
+import { ip as IPAddress, ipv6 } from 'address';
 import os, { NetworkInterfaceInfo } from "os";
 
 export function searchIPAddress(options: {
 	filterInterfacesNot?: string | RegExp | ((interfaceName: string, interfaceData: NetworkInterfaceInfo[]) => boolean);
 	filterIP?: string | RegExp | ((ip: string) => boolean);
 	defaultIP?: string,
+	family?: NetworkInterfaceInfo["family"]
 } = {})
 {
 	let interfaces = os.networkInterfaces();
 
-	let { filterInterfacesNot = defaultFilter, defaultIP = '127.0.0.1', filterIP } = options;
+	let { filterInterfacesNot = defaultFilter, defaultIP = '127.0.0.1', filterIP, family = 'IPv4' as NetworkInterfaceInfo["family"] } = options;
 
 	filterInterfacesNot = _handleInputCallback(filterInterfacesNot, defaultFilter);
-	filterIP = _handleInputCallback(filterIP, null);
+	filterIP = _handleInputCallback(filterIP, (ip) => ip && ip !== '127.0.0.1' && ip !== '0.0.0.0');
 
 	let fn = filterInterfacesNot;
 
@@ -26,11 +27,11 @@ export function searchIPAddress(options: {
 			{
 				if (bb)
 				{
-					return -1
+					return 1
 				}
 				else if (aa)
 				{
-					return 1
+					return -1
 				}
 			}
 
@@ -40,15 +41,27 @@ export function searchIPAddress(options: {
 
 	let ip: string;
 
+	let fns = family === 'IPv6' ? [
+		ipv6,
+		IPAddress,
+	]: [
+		IPAddress,
+		ipv6,
+	];
+
 	for (let interfaceName of keys)
 	{
-		try
+		for (let fn of fns)
 		{
-			ip = IPAddress(interfaceName);
-		}
-		catch (e)
-		{
+			try
+			{
+				ip = fn(interfaceName);
+				break;
+			}
+			catch (e)
+			{
 
+			}
 		}
 
 		if (ip && (!filterIP || filterIP(ip)))
@@ -67,7 +80,7 @@ export function searchIPAddress(options: {
 
 export function defaultFilter(interfaceName: string, interfaceData: NetworkInterfaceInfo[])
 {
-	return /vEthernet/.test(interfaceName)
+	return !/vEthernet/i.test(interfaceName) && interfaceData.some(data => !data.internal)
 }
 
 export function _handleInputCallback<T extends (s: string, ...argv: any) => boolean>(callback: string | RegExp | T, defaultCallback: T): T
